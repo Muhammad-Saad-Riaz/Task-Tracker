b14315abdaf0ea9c54af56b1e22ba2d3597569b4
Muhammad Saad Riaz <msaadriaz11@gmail.com>
2025-10-30 21:58:23 +0500

    Styling navbar in for small screens

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
  
   return (
     <div className=''>
-      <div className='sm:flex border-b justify-between p-5 bg-gray-900 text-white'>
-        <h1 className='ml-8 text-3xl font-bold text-center'>Task-Tracker</h1>
-        <nav className="flex items-center gap-4 mr-8">
+      <div className='w-[100%vw] sm:flex border-b justify-between p-5 bg-gray-900 text-white'>
+        <h1 className='sm:ml-8 text-3xl font-bold text-center mb-2'>Task-Tracker</h1>
+        <nav className="flex items-center justify-center gap-4 sm:mr-8">
           <Link className=' bg-gray-800 rounded-4xl hover:bg-gray-700 px-3 py-2' to='/'>View Tasks</Link>
           <Link className=' bg-gray-800 rounded-4xl hover:bg-gray-700 px-3 py-2' to='/new'>Add new Task</Link>
         </nav>
