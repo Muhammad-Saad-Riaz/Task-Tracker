e0999f727be806e6bf410c4ee5a619b02f6a4e6b
Muhammad Saad Riaz <msaadriaz11@gmail.com>
2025-10-30 21:44:09 +0500

    making navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Link,Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-
+ 
   return (
     <div className=''>
-      <div className='flex border-b justify-between p-5 bg-gray-900 text-white'>
+      <div className='sm:flex border-b justify-between p-5 bg-gray-900 text-white'>
         <h1 className='ml-8 text-3xl font-bold text-center'>Task-Tracker</h1>
         <nav className="flex items-center gap-4 mr-8">
           <Link className=' bg-gray-800 rounded-4xl hover:bg-gray-700 px-3 py-2' to='/'>View Tasks</Link>
